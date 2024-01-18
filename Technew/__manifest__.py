9d55aeca1ca7f2108b71daf613738f1b28ced545
Vishal Singh <68636367+vishallsinghh@users.noreply.github.com>
2024-01-18 12:38:06 +0530

    Add files via upload

---
+++ Technew/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Power BI Odoo Direct Connector",
+
+    'summary': """
+       	  
+Introducing the Direct Odoo Power BI Connector, the exclusive module that seamlessly integrates your Odoo platform with Power BI, offering enhanced features. Easily create Power BI Dashboards and Reports with automatic scheduled data refresh. This connector ensures a swift link between your Odoo data and the powerful Business Intelligence tool, Power BI Desktop.
+
+Experience advanced functionalities, such as publishing dashboards to Microsoft Teams or making them publicly accessible. Benefit from automatic scheduled refresh/reload, gateway support, and prebuilt tables relationship. This high-speed connector facilitates the transfer of data from Odoo to Power BI, providing an ER diagram-like feature to visualize relationships and columns in your tables.
+
+Explore a range of dashboards for Odoo ERP, including CRM, Inventory, Sales, Account, Invoice, and Revamp Dashboards. This connector is designed to revitalize your Odoo Dashboard, offering a comprehensive solution with features like Chart Graphs, Table Views, and all-in-one dynamic dashboards. It is a top-notch Odoo app in the market, ensuring a seamless connection between Odoo and Power BI for robust business intelligence.
+       """,
+
+    'description': """
+       
+Introducing the Direct Odoo Power BI Connector, the exclusive module that seamlessly integrates your Odoo platform with Power BI, offering enhanced features. Easily create Power BI Dashboards and Reports with automatic scheduled data refresh. This connector ensures a swift link between your Odoo data and the powerful Business Intelligence tool, Power BI Desktop.
+
+Experience advanced functionalities, such as publishing dashboards to Microsoft Teams or making them publicly accessible. Benefit from automatic scheduled refresh/reload, gateway support, and prebuilt tables relationship. This high-speed connector facilitates the transfer of data from Odoo to Power BI, providing an ER diagram-like feature to visualize relationships and columns in your tables.
+
+Explore a range of dashboards for Odoo ERP, including CRM, Inventory, Sales, Account, Invoice, and Revamp Dashboards. This connector is designed to revitalize your Odoo Dashboard, offering a comprehensive solution with features like Chart Graphs, Table Views, and all-in-one dynamic dashboards. It is a top-notch Odoo app in the market, ensuring a seamless connection between Odoo and Power BI for robust business intelligence.
+    """,
+        
+    'maintainer': 'Microsoft Special',
+    'author': "Microsoft Special",
+    'website': "https://bit.ly/3S55w4G",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '1.1',
+    'price': 0,
+    'currency': 'USD',
+    # any module necessary for this one to work correctly
+ 
+    'images': ['static/description/banner1.gif'],
+    "license":"LGPL-3",
+
+    # always loaded
+   
+    # only loaded in demonstration mode
+    "application": True,
+    "installable": True,
+     'data': [
+
+    ],
+   'depends': [],
+}
---
+++ Technew/readme.txt
@@ -0,0 +1,32 @@
+--------------------Odoo Power BI Direct Connector by Techneith-----------------------
+
+Thank you for downloading this free module of the best-selling Power BI Connector for Odoo 
+by Techneith
+
+We apologize for delay in the continuation of your purchase
+
+----------------------------------------------------------------------------------
+
+To get Odoo to Power BI Connector only for 49$,
+
+visit --> https://techneith.com/products/odoo-power-bi-direct-connector/ 
+
+Features offered by the connecter -
+
+    > Relatime Data Refresh
+    > Flexible Data Table Selection
+    > Live Demo
+    > Installation Guide 
+    >Free Installation
+
+Sale going on for lifetime purchase, visit now.
+---------------------------------------------------------------------------------
+
+Additional Products - 
+
+Odoo Data Model (View your Odoo Tables in Relationship Diagram)
+https://techneith.com/products/odoo-data-model/
+
+Odoo to Looker Connector(Connect Odoo to Google Looker Studio Directly)
+https://techneith.com/products/odoo-looker-studio/
+
